a436c617687806049f703a6174f52adbc7082811
随便 <42101994+zjm404@users.noreply.github.com>
2020-10-26 16:39:23 +0800

    Update DevEdit.java

--- java/src/main/java/cn/zjm404/stu/java/git/DevEdit.java
+++ java/src/main/java/cn/zjm404/stu/java/git/DevEdit.java
@@ -2,6 +2,6 @@ package cn.zjm404.stu.java.git;
 
 public class DevEdit {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("HELLO WORLD");
     }
 }
