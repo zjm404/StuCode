9beff858ccad9c6f50ed157018611318fa1b29d9
zjm <zjm404@foxmail.com>
2020-10-26 16:57:59 +0800

    dev edit test for stu diff

--- java/src/main/java/cn/zjm404/stu/java/git/DevEdit.java
+++ java/src/main/java/cn/zjm404/stu/java/git/DevEdit.java
@@ -3,5 +3,6 @@ package cn.zjm404.stu.java.git;
 public class DevEdit {
     public static void main(String[] args) {
         System.out.println("HELLO WORLD");
+        System.out.println("dev edit");
     }
 }
--- java/src/main/java/cn/zjm404/stu/java/git/DevEditDiff.java
+++
@@ -1,7 +0,0 @@
-package cn.zjm404.stu.java.git;
-
-public class DevEditDiff {
-    public static void main(String[] args) {
-        System.out.println("dev edit");
-    }
-}
